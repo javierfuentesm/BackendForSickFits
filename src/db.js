5b7438049ae0007f3f5e0d2bb591920aa89360a0
Javier <javier.fuentesm@hotmail.com>
2020-06-03 20:03:12 -0400

    Se implementa prisma

---
+++ src/db.js
@@ -0,0 +1,12 @@
+// This file connect to the remote prisa db and give us the ability to query it with js
+
+const { Prisma } = require("prisma-binding");
+
+const db = new Prisma({
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.exports = db;

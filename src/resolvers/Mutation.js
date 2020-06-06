f51c14406d03508d207ef53a907b210d809f7f80
Javier <javier.fuentesm@hotmail.com>
2020-06-05 21:00:19 -0400

    Primer query and mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  },
+};
 
 module.exports = Mutations;

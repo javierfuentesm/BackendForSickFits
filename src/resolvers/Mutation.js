eb467fecead98520eb0a149f39901033b406143d
Javier <javier.fuentesm@hotmail.com>
2020-06-06 22:38:15 -0400

    Se agregan primera mutacion y query para Item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,9 +1,10 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: { ...args },
+      info,
+    });
+    return item;
   },
 };
 

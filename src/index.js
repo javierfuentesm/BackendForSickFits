71f694bc72d462b6447b5c301111bc469ffc1982
Javier <javier.fuentesm@hotmail.com>
2020-07-14 22:56:42 -0400

    Currently Logged In User with Middleware and hooks

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const cookieParser = require("cookie-parser");
 const db = require("./db");
+const jwt = require("jsonwebtoken");
 
 const server = createServer();
 server.express.use(cookieParser());
+//decode the JWT so w can get the user on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    console.log(req.cookies);
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    console.log(userId);
+    //put the userId on to the request for future request to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -8,5 +8,12 @@ const Query = {
     const items = await ctx.db.query.items();
     return items;
   },*/
+  me(parent, args, ctx, info) {
+    //check if there is a current userId
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user({ where: { id: ctx.request.userId } }, info);
+  },
 };
 module.exports = Query;

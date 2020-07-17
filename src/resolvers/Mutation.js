8eb8c0b4eede5144fa91afec3cde603e0c93c4c2
Javier <javier.fuentesm@hotmail.com>
2020-07-16 22:37:23 -0400

    Sign in Form and Custom Error Handling

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -54,6 +54,28 @@ const Mutations = {
     });
     return user;
   },
+  async signin(parent, { email, password }, ctx, info) {
+    //Check if there is a user with that emal
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error("No such user found for email");
+    }
+    // Check if the password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid password");
+    }
+    //Gnerate the JWT Token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    //set the cookie with the token
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+
+    //Return the user
+    return user;
+  },
 };
 
 module.exports = Mutations;
